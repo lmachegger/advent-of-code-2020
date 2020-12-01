54280a871d1253b3905cdc03b812ef8864153b70
lmachegger <lmachegger@gmail.com>
2020-12-01 22:37:14 +0100

    added create_template.py, renamed day 1 files

---
+++ tools/create_template/README.md
@@ -0,0 +1,15 @@
+# Create Template
+
+This creates a folder `<foldername>` at the location `<location>`. Inside this folder are 2 empty input files, and 2 template python files, that read those input files.
+
+## Use this to create template:
+
+```
+python create_template.py <foldername> <location>
+```
+
+for example:
+
+```
+python create_template.py 3 ~/dev/advent-of-code-2020
+```
---
+++ tools/create_template/create_template.py
@@ -0,0 +1,37 @@
+import argparse
+import os
+
+# parse args
+parser = argparse.ArgumentParser()
+parser.add_argument("foldername", help="name of folder")
+parser.add_argument("location", help="where to create the template")
+args = parser.parse_args()
+
+# create dir
+path = os.path.join(args.location, args.foldername)
+os.mkdir(path)
+print('created directory ' + args.foldername)
+
+# create input files
+input_files = ["input1", "input2"]
+for inputf in input_files:
+    file_path = os.path.join(path, inputf)
+    f = open(file_path, 'x')
+    f.close()
+
+
+# create python files
+def get_lines(index):
+    return [
+        "#Day " + args.foldername + "\n",
+        "#Part " + index + "\n",
+        "f = open('input" + index + "', 'r')"
+    ]
+
+
+python_files = ["run1.py", "run2.py"]
+for i in range(len(python_files)):
+    file_path = os.path.join(path, python_files[i])
+    f = open(file_path, 'x')
+    f.writelines(get_lines(str(i + 1)))
+    f.close()
