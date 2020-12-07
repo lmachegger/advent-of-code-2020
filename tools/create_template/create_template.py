eb6c6e73995aae175c37b7e8c19219a518f0da4a
lmachegger <lmachegger@gmail.com>
2020-12-07 13:36:06 +0100

    updated create_template.py to fit new filename system

--- tools/create_template/create_template.py
+++ tools/create_template/create_template.py
@@ -12,12 +12,11 @@ path = os.path.join(args.location, args.foldername)
 os.mkdir(path)
 print('created directory ' + args.foldername)
 
-# create input files
-input_files = ["input1", "input2"]
-for inputf in input_files:
-    file_path = os.path.join(path, inputf)
-    f = open(file_path, 'x')
-    f.close()
+# create input file
+input_file = 'input' + args.foldername
+file_path = os.path.join(path, input_file)
+f = open(file_path, 'x')
+f.close()
 
 
 # create python files
@@ -25,13 +24,13 @@ def get_lines(index):
     return [
         "#Day " + args.foldername + "\n",
         "#Part " + index + "\n",
-        "f = open('input" + index + "', 'r')"
+        "with open('input" + args.foldername + "', 'r') as f:"
     ]
 
 
-python_files = ["part1.py", "part2.py"]
-for i in range(len(python_files)):
-    file_path = os.path.join(path, python_files[i])
+for i in range(0, 2):
+    file_name = 'day'+args.foldername+'_' + str(i+1) + '.py'
+    file_path = os.path.join(path, file_name)
     f = open(file_path, 'x')
     f.writelines(get_lines(str(i + 1)))
     f.close()
