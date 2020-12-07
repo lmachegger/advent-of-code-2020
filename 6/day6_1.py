616dda50ff78bcefbb19488049c4075970ea4643
lmachegger <lmachegger@gmail.com>
2020-12-07 17:03:27 +0100

    added day6

---
+++ 6/day6_1.py
@@ -0,0 +1,16 @@
+# Day 6
+# Part 1
+with open('input6', 'r') as f:
+    groups = f.read().split('\n\n')
+
+results = []
+for group in groups:
+    persons = group.split('\n')
+    answers = []
+    for person in persons:
+        for c in person:
+            if not c in answers:
+                answers.append(c)
+    results.append(len(answers))
+
+print(sum(results))
---
+++ 6/day6_2.py
@@ -0,0 +1,23 @@
+# Day 6
+# Part 2
+with open('input6', 'r') as f:
+    groups = f.read().split('\n\n')
+
+results = []
+for group in groups:
+    persons = group.split('\n')
+    answers = {}
+    for person in persons:
+        for c in person:
+            if not c in answers:
+                answers[c] = 1
+            else:
+                answers[c] += 1
+    result = 0
+    for answer in answers:
+        if answers[answer] == len(persons):
+            result += 1
+
+    results.append(result)
+
+print(sum(results))
