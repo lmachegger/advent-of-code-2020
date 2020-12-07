36269ee6f598488d6a4d688c23de0c4fefd3f609
lmachegger <lmachegger@gmail.com>
2020-12-07 16:49:06 +0100

    added day5

---
+++ 5/day5_1.py
@@ -0,0 +1,14 @@
+# Day 5
+# Part 1
+with open('input5', 'r') as f:
+    all_lines = [f.read().split('\n')][0]
+
+results = []
+
+for line in all_lines:
+    binary = line.replace('F', '0').replace(
+        'B', '1').replace('L', '0').replace('R', '1')
+    row, seat = binary[:7], binary[7:]
+    results.append(int(row, 2) * 8 + int(seat, 2))
+
+print(max(results))
